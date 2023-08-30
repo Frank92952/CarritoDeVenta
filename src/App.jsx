0e949b14574d5fb9785625e73559dfe4dd1fe9b6
Frank92952 <e_2019100823e@Uncp.edu.pe>
2023-08-29 23:11:17 -0500

    segunda arreglo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,84 @@
 import { useState } from 'react'
+import { RiMenuFill, RiUser3Fill, RiAddFill, RiCloseCircleFill, RiSearchEyeFill, RiArrowDownDoubleFill } from "react-icons/ri";
 import Sidebar from './components/shared/Sidebar'
 
 
 function App() {
-  
+  const [showMenu, setShowMenu]=useState(false);
+  const [showOrder, setShowOrder]=useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
   return (
     <>
    <div className='bg-[#262837] w-full min-h-screen'>
-      <Sidebar />
+      <Sidebar showMenu={showMenu} />
+
+    <nav className='bg-[#1F1D2B] lg:hidden fixed w-full bottom-0 left-0 text-3xl text-white py-2 px-8 flex items-center justify-between'>
+     <button className='p-2'>
+     <RiMenuFill />
+     </button>
+     <button className='p-2'>
+     <RiUser3Fill />
+     </button>
+     <button className='p-2'>
+     <RiAddFill />
+     </button>
+     <button onClick={toggleMenu} className='text-white p-2'>
+      {showMenu ? <RiCloseCircleFill />: <RiMenuFill />}
+     </button>
+    </nav>
+    <main className='lg:pl-28 grid grid-cols-1 lg:grid-cols-8 p-4'>
+      <div className='lg:col-span-6'>
+        {/* header */}
+        <header >
+          {/* title ande search */}
+          <div className='flex flex-col md:flex-row md:justify-between md:items-center gap-4 mb-6'>
+            <div>
+              <h1 className='text-2xl text-gray-300'>Venta De Ropa Deportiva</h1>
+              <p className='text-xl text-gray-500'>29 de Oct del 2023</p>
+            </div>
+            <form>
+              <div className='w-full relative'>
+                <RiSearchEyeFill className='absolute left-3 top-1/2 -translate-y-1/2 text-gray-300 '/>
+              <input type="text" className='bg-[#1F1D2B] w-full py-2 pl-8 pr-4 rounded-lg text-white outline-none' 
+              placeholder='Buscador'
+              />
+              </div>
+            </form>
+          </div>
+          {/* tabs */}
+          <nav className='text-gray-300 flex items-center justify-between md:justify-start md:gap-8 border-b'>
+            <a href='#' className='relative py-2 pr-4 before:w-1/2 before:h-[2px] before:absolute before:bg-blue-500 before:left-0 
+            before:rounded-full before:bottom-[-1px] text-blue-500'>Hot Dishes</a>
+            <a href='#' className=' py-2 pr-4 '>Hot Dishes</a>
+            <a href='#' className=' py-2 pr-4 '>Hot Dishes</a>
+            <a href='#' className=' py-2 pr-4'>Hot Dishes</a>
+      
+          </nav>
+
+        </header>
+          {/* title contents */}
+          <div className='flex items-center justify-between '>
+            <h2 className='text-xl text-gray-300'>Choose Distributs</h2>
+            <button className='flex items-center gap-4 text-gray-300 bg-[#1F1D2B] py-2 px-4 rounded-lg'>
+              <RiArrowDownDoubleFill /> Dine IN
+            </button>
+          </div>
+          {/* Content */}
+          <div>
+            <div>
+              {/* Card */}
+              
+            </div>
+
+          </div>
+      </div>
+      <div className='lg:col-span-2 fixed lg:static right-0'>
+        HOLA 2
+      </div>
+    </main>
    </div>
       
     
--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { RiHomeSmileFill,RiPercentFill,RiRestaurantFill, RiMailFill,RiNotification2Fill, RiSettings2Fill, RiLogoutBoxRFill } from "react-icons/ri";
+import { RiHomeSmileFill,RiPercentFill,RiShoppingCart2Fill, RiMailFill,RiNotification2Fill, RiSettings2Fill, RiLogoutBoxRFill } from "react-icons/ri";
 
-export default function Sidebar() {
+export default function Sidebar(props) {
+    const { showMenu } = props;
   return (
-    <div className='bg-[#1F1D2B] fixed left-0 top-0 w-28 h-full flex flex-col justify-between'>
+    <div className={`bg-[#1F1D2B] fixed lg:left-0 top-0 w-28 h-full flex flex-col justify-between rounded-tr-xl rounded-br-xl z-50 transition-all ${showMenu ? "left-0" : "-left-full"}`}
+    >
         <div>
-        <ul className='pl-4'>
+        <ul className='pl-6'>
             <li>
-            <h1 className='text-2xl text-gray-300 uppercase font-bold text-center my-5'>Logo</h1>
+            <h1 className='text-2xl text-gray-300 uppercase font-bold text-center my-6'>Logo</h1>
             </li>
             <li className='bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl '>
                 <a href='#' className='bg-blue-500 p-4 flex justify-center rounded-xl text-white'>
@@ -26,7 +28,7 @@ export default function Sidebar() {
             </li>
             <li className='hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors'>
                 <a href='#' className='group-hover:bg-blue-500 p-4 flex justify-center rounded-xl text-blue-500 group-hover:text-white transition-colors'>
-                    <RiRestaurantFill className='text-2xl' />
+                    <RiShoppingCart2Fill className='text-2xl' />
                 </a>
             </li>
             <li className='hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors'>
@@ -42,7 +44,7 @@ export default function Sidebar() {
         </ul>
         </div>
         <div>
-            <ul className='pl-4'>
+            <ul className='pl-6'>
                 <li className='hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors'>
                 <a href='#' className='group-hover:bg-blue-500 p-4 flex justify-center rounded-xl text-blue-500 group-hover:text-white transition-colors'>
                     <RiLogoutBoxRFill className='text-2xl' />
